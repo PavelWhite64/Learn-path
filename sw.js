1270aec13b8cd4fe3530f1297915af554dd23b1a
PavelWhite64 <123714644+PavelWhite64@users.noreply.github.com>
2025-10-05 20:12:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 // sw.js
-const CACHE_NAME = 'bridgelearn-v0.0.6';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/java/java-roadmap.html',
-  '/java/system/what-is-jdk.html'
-  // Добавь сюда другие страницы по мере создания:
-  // '/web/frontend-path.html',
-  // '/java/install-jdk.html'
-];
+//const CACHE_NAME = 'bridgelearn-v0.0.6';
+//const urlsToCache = [
+ // '/',
+ // '/index.html',
+ // '/java/java-roadmap.html',
+ // '/java/system/what-is-jdk.html'
+  //// Добавь сюда другие страницы по мере создания:
+  //// '/web/frontend-path.html',
+  //// '/java/install-jdk.html'
+//];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
+//self.addEventListener('install', (event) => {
+  //event.waitUntil(
+    //caches.open(CACHE_NAME)
+      //.then((cache) => cache.addAll(urlsToCache))
+  //);
+//});
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
+//self.addEventListener('fetch', (event) => {
+  //event.respondWith(
+    //caches.match(event.request)
+      //.then((response) => response || fetch(event.request))
+  //);
+//});
