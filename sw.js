e45cbe40d78dad9ffa5f70f58467315c8607011c
PavelWhite64 <123714644+PavelWhite64@users.noreply.github.com>
2025-10-05 13:19:29 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+const CACHE_NAME = 'bridgelearn-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+  // Добавь сюда другие страницы по мере создания:
+  // '/web/frontend-path.html',
+  // '/java/install-jdk.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
